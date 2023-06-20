78b39354493c82648255f9d74ab533046ff2f43f
Thomas <120122755+thomastour@users.noreply.github.com>
2023-06-20 22:14:57 +0300

    changes in html files

--- movieapp/movieapp/urls.py
+++ movieapp/movieapp/urls.py
@@ -17,7 +17,10 @@ Including another URLconf:
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("movies.urls")),
+    path("api-auth/", include("rest_framework.urls")),
+    
 ]
--- movieapp/movies/permissions.py
+++ movieapp/movies/permissions.py
@@ -1,6 +1,6 @@
 from rest_framework import permissions
 
-
+#custom permission class
 class IsOwnerOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         
